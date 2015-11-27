0b4d31c00d9f6f4c3617c71bc739dc8459c3e31c
alu0100697414 <alu0100697414@ull.edu.es>
2015-11-27 10:32:03 +0000

    Funcionando renderfile en preguntas de seleccion simple

--- models/pregunta_selec_simple.js
+++ models/pregunta_selec_simple.js
@@ -1,3 +1,5 @@
+var EJS = require('ejs');
+
 var Pregunta = require('../models/pregunta.js');
 
 function PreguntaSelecSimple(x, array) {
@@ -5,27 +7,26 @@ function PreguntaSelecSimple(x, array) {
 
   this.pregunta_ = x;
   this.array_ = array;
+  this.area_;
+
+  var self = this;
+
+  EJS.renderFile('views/quizes/pregSelecSimple.ejs',{array: this.array_}, function(err,html){
+    if(err) throw err;
+    else self.area_ = html;
+  });
 }
 
 PreguntaSelecSimple.prototype = new Pregunta();
 
 PreguntaSelecSimple.prototype.constructor = PreguntaSelecSimple;
 
-PreguntaSelecSimple.prototype.vista = function(){
-  var vista = [];
-
-  for(var i=0; i<this.array_.length; i++){
-    vista[i] = "<option>" + this.array_[i] + "</option>";
-  }
-
-  vista.unshift("<select name='respuesta'>");
-  vista.push("</select>");
-
-  return vista;
-}
-
 PreguntaSelecSimple.prototype.get_pregunta = function(){
   return this.pregunta_;
+}
+
+PreguntaSelecSimple.prototype.get_area = function(){
+  return this.area_;
 }
 
 module.exports = PreguntaSelecSimple;
